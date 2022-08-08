f0610053843b3dc6bcc1f86c85032630d18cbdb8
diasleo98 <leodias98@hotmail.com>
2022-08-08 14:57:30 -0300

    test

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,7 +1,11 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default (req, res) => {
-  console.log(req.body)
-  
-  res.status(200).json({ name: 'John Doe' })
+  if(req.method === 'POST'){
+    res.status(200).json(req.body)
+  }
+  else{
+    res.status(200).json({ name: 'John Doe' })
+
+  }
 }
