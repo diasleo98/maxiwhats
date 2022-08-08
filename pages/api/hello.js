d4eda9305d526dea623f08f3e145101bcd8ba0d2
diasleo98 <leodias98@hotmail.com>
2022-08-08 12:33:38 -0300

    last

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,6 +1,8 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default (req, res) => {
-  console.log(req)
+  console.log(req.body)
+  console.log(req.params)
+  console.log(req.get('Content-Type'))
   res.status(200).json({ name: 'John Doe' })
 }
